f1aaf649f0facf55df6007205479e3ac52d0d7c2
Junaid Aslam <jaslam168@gmail.com>
2024-01-29 15:58:15 +0500

    Created react app "Initial commit"

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hey my self Junaid Aslam Tanoli and I am a Digital Software Engineer.
+        </p>
+        <a href="#">My Portfolio</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
